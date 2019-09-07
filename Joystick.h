43784b4b9c637ae3b5ac4f4dc07c83a3720cf4c5
Zuhair Rayyes <rayyes95@gmail.com>
2019-09-07 13:22:32 +0200

    broken

--- Joystick.h
+++ Joystick.h
@@ -117,6 +117,7 @@ void EVENT_USB_Device_Connect(void);
 void EVENT_USB_Device_Disconnect(void);
 void EVENT_USB_Device_ConfigurationChanged(void);
 void EVENT_USB_Device_ControlRequest(void);
+void ChangeColorIndex(void);
 // Prepare the next report for the host.
 void GetNextReport(USB_JoystickReport_Input_t* const ReportData);
 
